b0223e9478b1b0dc3637e52363f1d6932fbfc0e2
Anthony Moe <anthony.moe7@gmail.com>
2021-04-06 16:25:35 -0500

    have .env

--- server/modules/routes/giphy_router.js
+++ server/modules/routes/giphy_router.js
@@ -2,10 +2,13 @@ const express = require( 'express' );
 const router = express.Router();
 // axios on server
 const axios = require( 'axios' );
+// allows a connection to .env file
+require( 'dotenv' ).config();
+
 router.get( '/', ( req, res )=>{
     console.log( '/giphy GET' );
     // make an API call to giphy's server with our API key
-    axios.get( `https://api.giphy.com/v1/gifs/trending?api_key=baaA9KVlrbw8fKdKshJvAzzSGpqkrQHa&limit=10` ).then( ( response )=>{
+    axios.get( `https://api.giphy.com/v1/gifs/trending?api_key=${process.env.GIPHY_API_KEY}&limit=10` ).then( ( response )=>{
         res.send( response.data );
     }).catch( ( err )=>{
         console.log( err );
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,18 @@
-import react from 'react';
+
 import axios from 'axios';
 import './App.css';
 
 function App () {
-
-  function getGiphs() {
-    axios.get('/')
-  }
+  
+  const getGifs = () =>{
+    console.log( 'in getGifs' );
+    axios.get( '/giphy' ).then( ( response )=>{
+      console.log( 'back from GET:', response.data );
+    }).catch( ( err )=>{
+      alert( 'err' );
+      console.log( err );
+    })
+  } 
 
     return (
       <div className="App">
@@ -14,7 +20,7 @@ function App () {
           <h1 className="App-title">APIS</h1>
           <h4><i>APIS</i></h4>
         </header>
-        <button onClick={getGiphs}>Get gifs</button>
+        <button onClick={getGifs}>Get gifs</button>
         <br/>
       </div>
     );
